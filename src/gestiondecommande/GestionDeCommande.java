9787c17760ad70eaa5f1ceef5bed1de42a86621b
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2013-02-21 14:38:00 -0500

    nouveau projet netbeans

---
+++ src/gestiondecommande/GestionDeCommande.java
@@ -0,0 +1,7 @@
+package gestiondecommande;
+
+public class GestionDeCommande {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ test/gestiondecommande/GestionDeCommandeTest.java
@@ -0,0 +1,16 @@
+package gestiondecommande;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GestionDeCommandeTest {
+
+    public GestionDeCommandeTest() {
+    }
+    
+    @Test
+    public void test(){}
+}
